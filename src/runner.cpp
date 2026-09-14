59d8374bb3801d10aa0899dd08ef46022f27c7a7
dy-dev <dominique@arcreane.com>
2026-09-14 18:18:39 +0100

    S2 starter : CMakeLists minimal + runner.cpp squelette + gitignore C++

---
+++ src/runner.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    // TODO : menu de démarrage (Jouer / Quitter)
+
+    // TODO : boucle de jeu si le joueur choisit Jouer
+
+    return 0;
+}
